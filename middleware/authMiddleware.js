808849aca7f49a7d6cf6c6d2ce3550bee03723e2
CiceroLucas <lukasferreiradesousa890@gmail.com>
2023-10-15 21:27:20 -0300

    feat: add middleware authentication

---
+++ middleware/authMiddleware.js
@@ -0,0 +1,16 @@
+const jwt = require("jsonwebtoken");
+
+exports.checkToken = async (req, res, next) => {
+    const authHeader = req.headers['authorization'];
+    const token = authHeader && authHeader.split(" ")[1];
+
+    if(!token) return res.status(401).json({ message : "Acesso negado!"});
+
+    try {
+        const secret = process.env.SECRET
+        jwt.verify(token, secret)
+        next()
+    } catch (error) {
+        res.status(400).json({ message: "Token inválido"})
+    }
+};

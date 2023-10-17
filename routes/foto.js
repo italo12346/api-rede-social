161c29a782ae79c886b235dd5c9d2acc11031390
CiceroLucas <lukasferreiradesousa890@gmail.com>
2023-10-17 10:27:02 -0300

    feat: add middleware foto upload

--- routes/foto.js
+++ routes/foto.js
@@ -1,10 +1,11 @@
 // routes/foto.js
 const express = require("express");
 const router = express.Router();
+const upload = require("../config/multer");
 const fotoController = require("../controllers/fotoController");
 const authController = require("../middleware/authMiddleware");
 
-router.post("/create", authController.checkToken, fotoController.createFoto);
+router.post("/create", authController.checkToken, upload.single('imagem'), fotoController.createFoto);
 router.get("/list", authController.checkToken, fotoController.listFotos);
 router.get("/:id", authController.checkToken, fotoController.getFotoById);
 router.put("/:id", authController.checkToken, fotoController.updateFotoById);

505acfc674b0350f62bb88eb532f4dae761db85c
CiceroLucas <lukasferreiradesousa890@gmail.com>
2023-10-17 08:42:33 -0300

    feat: add middleware profilepic upload

--- routes/user.js
+++ routes/user.js
@@ -1,11 +1,12 @@
 // routes/user.js
 const express = require("express");
 const router = express.Router();
+const upload = require("../config/multer");
 const userController = require("../controllers/userController");
 const authController = require("../middleware/authMiddleware");
 
-router.post("/create", userController.createUser);
-router.post("/login", userController.login)
+router.post("/create",  upload.single('fotoPerfil'), userController.createUser);
+router.post("/login", userController.login);
 router.get("/list", authController.checkToken, userController.listUsers);
 router.get("/:id", authController.checkToken, userController.getUserById);
 router.put("/:id", authController.checkToken, userController.updateUserById);

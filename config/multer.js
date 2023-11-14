ecfe8902c2c6a4891d1234700c247f081f2167f4
CiceroLucas <lukasferreiradesousa890@gmail.com>
2023-11-14 13:49:22 -0300

    update package

--- config/multer.js
+++ config/multer.js
@@ -1,11 +1,12 @@
-const multer = require('multer');
+const multer = require("multer");
+const path = require("path");
 
 const storage = multer.diskStorage({
   destination: function (req, file, cb) {
-    cb(null, 'public/uploads'); 
+    cb(null, "public/uploads");
   },
   filename: function (req, file, cb) {
-    cb(null, Date.now() + '-' + file.originalname);
+    cb(null, Date.now() + path.extname(file.originalname));
   },
 });
 
